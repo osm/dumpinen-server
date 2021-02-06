5e8ff0e64f230f0691ef2ba0c1a9d924b5bbd107
Oscar Linderholm <osm@recv.se>
2021-02-06 08:37:14 +0100

    Fix man

--- man.go
+++ man.go
@@ -3,18 +3,18 @@ package main
 const man = `File dump service
 
 # Dump "foo".
-$ echo "foo" | curl --data-binary @- https://dumpinen.com
+echo "foo" | curl --data-binary @- https://dumpinen.com
 WGBtm-RLJkE
 
 # Get dump.
 curl https://dumpinen.com/WGBtm-RLJkE
 
 # Dump "foo" and delete it after ten minutes.
-$ echo "foo" | curl --data-binary @- https://dumpinen.com?deleteAfter=10m
+echo "foo" | curl --data-binary @- https://dumpinen.com?deleteAfter=10m
 Tuo3wgzdBVX
 
 # Dump "foo" and password protect it.
-$ echo "foo" | curl --data-binary @- --user foo:bar https://dumpinen.com
+echo "foo" | curl --data-binary @- --user foo:bar https://dumpinen.com
 NbbMcLcGcA9
 
 # Get the password protected dump.
